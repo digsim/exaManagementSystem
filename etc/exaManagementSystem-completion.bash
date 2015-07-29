a00227c34abd6557d9aa4a002b76fda696efe967
Andreas Ruppen <andreas.ruppen@gmail.com>
2015-07-29 18:03:34 +0200

    Minor fixes for bash-completion

--- etc/exaManagementSystem-completion.bash
+++ etc/exaManagementSystem-completion.bash
@@ -5,10 +5,14 @@ _exaManagementSystem()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
     opts="--make-new-exercise --build-exam --build-all-exams --make-workbook --make-catalogue --preview-exercise --preview-solution --make-new-lecture"
-    optssimple="-e -s -u -k -t"
-    exercices=`ls Exercices/ |awk -F "ex" '{print $2}'| sort -g`
+    optssimple="-e -s -u -k -t -l"
     
     case "${prev}" in
+        --make-new-lecture)
+            local addopts="-l"
+            COMPREPLY=( $(compgen -W "${addopts}" -- ${cur}) )
+            return 0
+            ;;
         --build-exam)
             local addopts="-s"
             COMPREPLY=( $(compgen -W "${addopts}" -- ${cur}) ) 
@@ -30,7 +34,6 @@ _exaManagementSystem()
            return 0
            ;;
        -e)
-	   #local addopts=`ls Exercices/ |awk -F "ex" '{print $2}'| sort -g`
            local addopts=`ls Exercices/ |awk -F "ex" '{print $2}'| sed -e 's/\^\[\[0m\$//g'| sort -g | sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g' `
            COMPREPLY=( $(compgen -W "${addopts}" -- ${cur}) ) 
            return 0
